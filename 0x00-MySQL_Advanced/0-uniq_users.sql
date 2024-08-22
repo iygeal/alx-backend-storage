57f0c9ce9432e3e4ea9ddc18b544247fd0fa0449
Innocent Anozie <iygeal@gmail.com>
2024-08-22 10:07:02 +0100

    A SQL script that creates a table called users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+--- A SQL script that creates a table called users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
