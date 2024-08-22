89c9d8da99d03796b57aff5d42fbd6d4e83dd9da
Innocent Anozie <iygeal@gmail.com>
2024-08-22 16:34:56 +0100

    TASK 2: Sum number of fans and return as nb_fans, sort and return data as required

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,16 @@
+-- Task 2: Write a SQL script that ranks country origins of bands, ordered by the number of fans
+-- The table dump `metal_bands.sql` should be imported before running this script
+-- The results should include the columns `origin` and `nb_fans`
+-- The script should be executable on any database
+
+SELECT
+    origin,
+    SUM(nb_fans) AS nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC;
+
+-- End of Task 2 script
