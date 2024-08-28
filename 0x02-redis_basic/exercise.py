1e5d3c224e1c7d65df3b8339a8604167fad28c12
Innocent Anozie <iygeal@gmail.com>
2024-08-28 20:26:33 +0100

    TASK 1: Added more methods to manage data retrieval and type conversion

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ used to interact with a Redis database
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,3 +25,43 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[
+            Callable] = None) -> Optional[Union[str, bytes, int, float]]:
+        """Retrieves data from Redis and optionally converts it using fn
+
+        Args:
+            key (str): The key to retrieve the data for
+            fn (Optional[ Callable], optional): Function to convert the data.
+            Defaults to None.
+
+        Returns:
+            Optional[Union[str, bytes, int, float]]: Retrieved data or None
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+
+        def get_str(self, key: str) -> Optional[str]:
+            """Retrieve data from Redis and convert it to a UTF-8 string
+
+            Args:
+                key (str): The key to retrieve the data for
+
+            Returns:
+                Optional[str]: Retrieved data as a string or None
+            """
+            return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+        def get_int(self, key: str) -> Optional[int]:
+            """Retrieves data from Redis and converts it to an integer
+
+            Args:
+                key (str): The key to retrieve the data for
+
+            Returns:
+                Optional[int]: The retrieved data as an integer or None
+            """
+            return self.get(key, fn=int)
